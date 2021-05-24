de9c776576c1691f81ba849dc4915653219e99b3
Loki1204 <loki1204@users.noreply.github.com>
2021-05-24 16:14:23 +0530

    commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const mongodb = require("mongodb");
 
-const port = process.env.port || 4000;
+const port = process.env.PORT || 4000;
 const app = express();
 
 const mongoClient = mongodb.MongoClient;
